191d73c4dcc23f704a207c83e1faa27b13a21b38
thanhoangxuannghiep <thanhoangxuannghiep@gmail.com>
2017-04-13 18:47:38 +0700

    edit cors

--- Web Service/App_Start/WebApiConfig.cs
+++ Web Service/App_Start/WebApiConfig.cs
@@ -10,7 +10,7 @@ namespace Web_Service
     {
         public static void Register(HttpConfiguration config)
         {
-            var cors = new EnableCorsAttribute("http://cookbookweb.apphb.com", "*", "*");
+            var cors = new EnableCorsAttribute("http://cookbook-20.apphb.com", "*", "*");
             config.EnableCors(cors);
 
             //config.EnableCors(new EnableCorsAttribute("http://localhost:1188", "*", "*"));
